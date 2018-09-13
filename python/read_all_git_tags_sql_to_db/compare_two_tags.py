cb01661470e6973e3b3ce00a0df1bd039e998fd2
jared-ong <jong@mdsol.com>
2018-09-13 04:24:02 +0000

    Pylint refactor
    Pylint refactor

--- python/read_all_git_tags_sql_to_db/compare_two_tags.py
+++ python/read_all_git_tags_sql_to_db/compare_two_tags.py
@@ -1,201 +1,181 @@
+#compare_tow_tags.py
 import urllib
-from sqlalchemy import create_engine
-from bs4 import UnicodeDammit
-import pandas as pd
-import numpy as np
 import re
+import pandas as pd
+from sqlalchemy import create_engine
 
-#pyodbc connection params and engine creation for later df to sql
-params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};SERVER=localhost,2017;DATABASE=schema_changes;UID=python_user;PWD=python_user")
-engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
-sql_table = "develop_branch_sql"
+#pyodbc connection PARAMS and ENGINE creation for later df to sql
+PARAMS = urllib.parse.quote_plus(
+        "DRIVER={SQL Server Native Client 11.0};SERVER=localhost,2017;DATABASE=schema_changes;UID=python_user;PWD=python_user")
+ENGINE = create_engine("mssql+pyodbc:///?odbc_connect=%s" % PARAMS)
+SQL_TABLE = "develop_branch_sql"
 
-tag1 = "v2017.2.0"
-tag2 = "v2018.1.3"
+TAG1 = "v2017.2.0"
+TAG2 = "v2018.1.3"
 
-query1 = "select * from {table} where tag = '{tag}'".format(table=sql_table,tag=tag1)
-query2 = "select * from {table} where tag = '{tag}'".format(table=sql_table,tag=tag2)
-
-df1 = pd.read_sql(query1, engine)
-df2 = pd.read_sql(query2, engine)
-
-#delete the second duplicate file_content_hash values from each dataframe. Ex. 20100920-01-RemoveTaggingReports.sql, 20100920-02-RemoveTaggingReports.sql, 20170404-01-Create_missing_indexes_on_FK.sql, 20110506-01-Table_CoderFieldConfigurationWorkFlowDataAddUpdatedAndCreated.sql, 20110516-25-ProjectCoderWorkFlowDataAddUpdatedAndCreated.sql, Create_missing_indexes_on_FK.sql
-df1 = df1.drop_duplicates(subset='file_content_hash',keep='first')
-df2 = df2.drop_duplicates(subset='file_content_hash',keep='first')
-
-#remove directory paths not like Rave_Viper_Lucy_Merged_DB_Scripts
-df1 = df1[df1['dir_path'].str.contains('Rave_Viper_Lucy_Merged_DB_Scripts')]
-#remove certain directory paths the ~ is the opposite result set
-df1 = df1[~df1['dir_path'].str.contains('tSQLt_UnitTests')]
-df1 = df1[~df1['dir_path'].str.contains('Samples')]
-df1 = df1[~df1['dir_path'].str.contains('SolarWinds')]
-df1 = df1[~df1['dir_path'].str.contains('Registry1')]
-df1 = df1[~df1['dir_path'].str.contains('TSDV DB Install Scripts')]
-
-#remove directory paths not like Rave_Viper_Lucy_Merged_DB_Scripts
-df2 = df2[df2['dir_path'].str.contains('Rave_Viper_Lucy_Merged_DB_Scripts')]
-#remove certain directory paths the ~ is the opposite result set
-df2 = df2[~df2['dir_path'].str.contains('tSQLt_UnitTests')]
-df2 = df2[~df2['dir_path'].str.contains('Samples')]
-df2 = df2[~df2['dir_path'].str.contains('SolarWinds')]
-df2 = df2[~df2['dir_path'].str.contains('Registry1')]
-df2 = df2[~df2['dir_path'].str.contains('TSDV DB Install Scripts')]
-
-
-#get 2 columns from each df
-df1part = df1.loc[:,['file_name','file_content_hash']]
-df2part = df2.loc[:,['file_name','file_content_hash']]
-
-
-#get list of unchanged files based on exact hash match
-df2_files_unchanged = pd.merge(df2part, df1part,  how='inner', left_on=['file_content_hash'], right_on = ['file_content_hash'])
-df2_files_unchanged.columns = ['file_name','file_content_hash','file_name_y']
-df2_files_unchanged = df2_files_unchanged.drop(columns=['file_name_y'])
-
-#get list of new files only
-#left join on file_name
-df2_files_new = pd.merge(df2part,df1part,how='left',on='file_name')
-#only return rows where they did not exist in tag1
-df2_files_new = df2_files_new.loc[df2_files_new.notna()['file_content_hash_y']==False]
-#rename the columns after join and drop file_content_hash_y column
-df2_files_new = df2_files_new.drop(columns=['file_content_hash_y'])
-df2_files_new.columns = ['file_name','file_content_hash']
-#finally make sure does not exist in the files unchanged list
-df2_files_new = pd.merge(df2_files_new,df2_files_unchanged,how='left',left_on=['file_content_hash'],right_on=['file_content_hash'])
-df2_files_new = df2_files_new.loc[df2_files_new.notna()['file_name_y']==False]
-df2_files_new = df2_files_new.drop(columns=['file_name_y'])
-df2_files_new.columns = ['file_name','file_content_hash']
-
-
-#get list of files changed only by removing unchanged and new files
-#exclude unchanged files first
-df2_files_changed = pd.merge(df2part,df2_files_unchanged, how='left',left_on=['file_content_hash'], right_on = ['file_content_hash'])
-df2_files_changed = df2_files_changed.loc[df2_files_changed.notna()['file_name_y']==False]
-df2_files_changed = df2_files_changed.drop(columns=['file_name_y'])
-df2_files_changed.columns = ['file_name','file_content_hash']
-
-#exclude new files
-df2_files_changed = pd.merge(df2_files_changed,df2_files_new, how='left',left_on=['file_content_hash'], right_on = ['file_content_hash'])
-df2_files_changed = df2_files_changed.loc[df2_files_changed.notna()['file_name_y']==False]
-df2_files_changed = df2_files_changed.drop(columns=['file_name_y'])
-df2_files_changed.columns = ['file_name','file_content_hash']
-
-#join back to the original df2
-df2_files_unchanged_all = pd.merge(df2_files_unchanged, df2, how='inner', left_on=['file_content_hash','file_name'], right_on= ['file_content_hash','file_name'])
-df2_files_changed_all = pd.merge(df2_files_changed,df2,how='inner')
-df2_files_new_all = pd.merge(df2_files_new,df2,how='inner')
-
-def remove_empty_lists(l):
+def remove_empty_lists(the_list):
     #new list
     newlist = []
     #loop over elements
-    for i in l:
+    for i in the_list:
         #pdb.set_trace()
         #is element a non-empty list? then call self on it
-        if isinstance(i, list) and len(i) != 0:
-            newlist.append(remover(i))
+        if isinstance(i, list) and i:
+            newlist.append(remove_empty_lists(i))
         #if not a list
         if not isinstance(i, list):
             newlist.append(i)
     return newlist
 
-def process_DDL(file_content):
+#find all ddl statements in a sql string
+def process_ddl(file_content):
     #Procedures
-    find=[]
+    find = []
     regex = r"CREATE\s+PROCEDURE\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"DROP\s+PROCEDURE\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"CREATE\s+PROC\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"DROP\s+PROC\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2    
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"ALTER\s+PROCEDURE\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     #Views
     regex = r"CREATE\s+VIEW\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"DROP\s+VIEW\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"ALTER\s+VIEW\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     #Tables
     regex = r"CREATE\s+TABLE\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"DROP\s+TABLE\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"ALTER\s+TABLE\s+[a-zA-Z0-9_\[\].]+\s+ALTER\s+COLUMN.*"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"ALTER\s+TABLE\s+[a-zA-Z0-9_\[\].]+\s+ADD.*"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     #Functions
     regex = r"CREATE\s+FUNCTION\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"DROP\s+FUNCTION\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"ALTER\s+FUNCTION\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     #Types
     regex = r"CREATE\s+TYPE\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"DROP\s+TYPE\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"ALTER\s+TYPE\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2    
+    find = find + re.findall(regex, file_content, re.I)
+    #Triggers
+    regex = r"CREATE\s+TRIGGER\s+[a-zA-Z0-9_\[\].]+"
+    find = find + re.findall(regex, file_content, re.I)
+    regex = r"DROP\s+TRIGGER\s+[a-zA-Z0-9_\[\].]+"
+    find = find + re.findall(regex, file_content, re.I)
+    regex = r"ALTER\s+TRIGGER\s+[a-zA-Z0-9_\[\].]+"
+    find = find + re.findall(regex, file_content, re.I)
     #INDEXES
     regex = r"CREATE\s+INDEX\s+[a-zA-Z0-9_\[\].]+\s+ON\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"CREATE\s+CLUSTERED\s+INDEX\s+[a-zA-Z0-9_\[\].]+\s+ON\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"CREATE\s+NONCLUSTERED\s+INDEX\s+[a-zA-Z0-9_\[\].]+\s+ON\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"CREATE\s+UNIQUE\s+INDEX\s+[a-zA-Z0-9_\[\].]+\s+ON\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2    
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"DROP\s+INDEX\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find = find + find2
+    find = find + re.findall(regex, file_content, re.I)
     regex = r"ALTER\s+INDEX\s+[a-zA-Z0-9_\[\].]+"
-    find2 = re.findall(regex, file_content, re.I)    
-    find.append(find2) 
-    find = remove_empty_lists(find) 
+    find = find + re.findall(regex, file_content, re.I)
+    find = remove_empty_lists(find)
     return find
 
-df2_files_new_all['ddl'] = ""
-for index, row in df2_files_new_all.iterrows():
-    the_file_content = df2_files_new_all.iloc[index]['file_content']
-    theddl = process_DDL(the_file_content)
-    print(df2_files_new_all.iloc[index]['full_path'])
-    print(theddl)
-    df2_files_new_all.at[index,'ddl'] = theddl
-    
-df2_files_changed_all['ddl'] = ""
-for index, row in df2_files_changed_all.iterrows():
-    the_file_content = df2_files_changed_all.iloc[index]['file_content']
-    theddl = process_DDL(the_file_content)
-    print(df2_files_changed_all.iloc[index]['full_path'])
-    print(theddl)
-    df2_files_changed_all.at[index,'ddl'] = theddl
+QUERY1 = "select * from {table} where tag = '{tag}'".format(table=SQL_TABLE, tag=TAG1)
+QUERY2 = "select * from {table} where tag = '{tag}'".format(table=SQL_TABLE, tag=TAG2)
+
+DF1 = pd.read_sql(QUERY1, ENGINE)
+DF2 = pd.read_sql(QUERY2, ENGINE)
+
+#delete the second duplicate file_content_hash values from each dataframe. Same file different name.
+DF1 = DF1.drop_duplicates(subset='file_content_hash', keep='first')
+DF2 = DF2.drop_duplicates(subset='file_content_hash', keep='first')
+
+#remove directory paths not like Rave_Viper_Lucy_Merged_DB_Scripts
+DF1 = DF1[DF1['dir_path'].str.contains('Rave_Viper_Lucy_Merged_DB_Scripts')]
+#remove certain directory paths the ~ is the opposite result set
+DF1 = DF1[~DF1['dir_path'].str.contains('tSQLt_UnitTests')]
+DF1 = DF1[~DF1['dir_path'].str.contains('Samples')]
+DF1 = DF1[~DF1['dir_path'].str.contains('SolarWinds')]
+DF1 = DF1[~DF1['dir_path'].str.contains('Registry1')]
+DF1 = DF1[~DF1['dir_path'].str.contains('TSDV DB Install Scripts')]
+
+#remove directory paths not like Rave_Viper_Lucy_Merged_DB_Scripts
+DF2 = DF2[DF2['dir_path'].str.contains('Rave_Viper_Lucy_Merged_DB_Scripts')]
+#remove certain directory paths the ~ is the opposite result set
+DF2 = DF2[~DF2['dir_path'].str.contains('tSQLt_UnitTests')]
+DF2 = DF2[~DF2['dir_path'].str.contains('Samples')]
+DF2 = DF2[~DF2['dir_path'].str.contains('SolarWinds')]
+DF2 = DF2[~DF2['dir_path'].str.contains('Registry1')]
+DF2 = DF2[~DF2['dir_path'].str.contains('TSDV DB Install Scripts')]
 
 
+#get 2 columns from each df
+DF1PART = DF1.loc[:, ['file_name', 'file_content_hash']]
+DF2PART = DF2.loc[:, ['file_name', 'file_content_hash']]
+
+
+#get list of unchanged files based on exact hash match
+DF2_FILES_UNCHANGED = pd.merge(DF2PART, DF1PART, how='inner', left_on=['file_content_hash'], right_on= ['file_content_hash'])
+DF2_FILES_UNCHANGED.columns = ['file_name', 'file_content_hash', 'file_name_y']
+DF2_FILES_UNCHANGED = DF2_FILES_UNCHANGED.drop(columns=['file_name_y'])
+
+#get list of new files only
+#left join on file_name
+DF2_FILES_NEW = pd.merge(DF2PART, DF1PART, how='left', on='file_name')
+#only return rows where they did not exist in TAG1
+DF2_FILES_NEW = DF2_FILES_NEW.loc[DF2_FILES_NEW.notna()['file_content_hash_y'] == False]
+#rename the columns after join and drop file_content_hash_y column
+DF2_FILES_NEW = DF2_FILES_NEW.drop(columns=['file_content_hash_y'])
+DF2_FILES_NEW.columns = ['file_name', 'file_content_hash']
+#finally make sure does not exist in the files unchanged list
+DF2_FILES_NEW = pd.merge(DF2_FILES_NEW, DF2_FILES_UNCHANGED, how='left', left_on=['file_content_hash'], right_on=['file_content_hash'])
+DF2_FILES_NEW = DF2_FILES_NEW.loc[DF2_FILES_NEW.notna()['file_name_y'] == False]
+DF2_FILES_NEW = DF2_FILES_NEW.drop(columns=['file_name_y'])
+DF2_FILES_NEW.columns = ['file_name', 'file_content_hash']
+
+#get list of files changed only by removing unchanged and new files
+#exclude unchanged files first
+DF2_FILES_UNCHANGED = pd.merge(DF2PART, DF2_FILES_UNCHANGED, how='left', left_on=['file_content_hash'], right_on= ['file_content_hash'])
+DF2_FILES_UNCHANGED = DF2_FILES_UNCHANGED.loc[DF2_FILES_UNCHANGED.notna()['file_name_y'] == False]
+DF2_FILES_UNCHANGED = DF2_FILES_UNCHANGED.drop(columns=['file_name_y'])
+DF2_FILES_UNCHANGED.columns = ['file_name', 'file_content_hash']
+
+#exclude new files
+DF2_FILES_UNCHANGED = pd.merge(DF2_FILES_UNCHANGED,DF2_FILES_NEW, how='left', left_on=['file_content_hash'], right_on= ['file_content_hash'])
+DF2_FILES_UNCHANGED = DF2_FILES_UNCHANGED.loc[DF2_FILES_UNCHANGED.notna()['file_name_y'] == False]
+DF2_FILES_UNCHANGED = DF2_FILES_UNCHANGED.drop(columns=['file_name_y'])
+DF2_FILES_UNCHANGED.columns = ['file_name', 'file_content_hash']
+
+#join back to the original DF2
+DF2_FILES_UNCHANGED_ALL = pd.merge(DF2_FILES_UNCHANGED, DF2, how='inner')
+DF2_FILES_NEW_ALL = pd.merge(DF2_FILES_NEW, DF2, how='inner')
+
+DF2_FILES_NEW_ALL['ddl'] = ""
+for index, row in DF2_FILES_NEW_ALL.iterrows():
+    the_file_content = DF2_FILES_NEW_ALL.iloc[index]['file_content']
+    theddl = process_ddl(the_file_content)
+    print(DF2_FILES_NEW_ALL.iloc[index]['full_path'])
+    print(theddl)
+    DF2_FILES_NEW_ALL.at[index, 'ddl'] = theddl
+
+DF2_FILES_UNCHANGED_ALL['ddl'] = ""
+for index, row in DF2_FILES_UNCHANGED_ALL.iterrows():
+    the_file_content = DF2_FILES_UNCHANGED_ALL.iloc[index]['file_content']
+    theddl = process_ddl(the_file_content)
+    print(DF2_FILES_UNCHANGED_ALL.iloc[index]['full_path'])
+    print(theddl)
+    DF2_FILES_UNCHANGED_ALL.at[index, 'ddl'] = theddl
--- python/read_all_git_tags_sql_to_db/main.py
+++ python/read_all_git_tags_sql_to_db/main.py
@@ -6,18 +6,18 @@ import git
 import read_sql_files
 import pandas as pd
 
-#pyodbc connection params and engine creation for later df to sql
-db_connect_string = "DRIVER={SQL Server Native Client 11.0};SERVER=localhost,2017;DATABASE=schema_changes;UID=python_user;PWD=python_user"
-params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};SERVER=localhost,2017;DATABASE=schema_changes;UID=python_user;PWD=python_user")
-engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
-sql_table = "develop_branch_sql"
+#pyodbc connection PARAMS and ENGINE creation for later df to sql
+DB_CONNECT_STRING = "DRIVER={SQL Server Native Client 11.0};SERVER=localhost,2017;DATABASE=schema_changes;UID=python_user;PWD=python_user"
+PARAMS = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 11.0};SERVER=localhost,2017;DATABASE=schema_changes;UID=python_user;PWD=python_user")
+ENGINE = create_engine("mssql+pyodbc:///?odbc_connect=%s" % PARAMS)
+SQL_TABLE = "develop_branch_sql"
 
 #directory where the .sql files are
-var_directory_to_sql = 'C:\\Users\\jong\\Documents\\GitHub\\Rave\\Medidata 5 RAVE Database Project'
+SQL_DIRECTORY = 'C:\\Users\\jong\\Documents\\GitHub\\Rave\\Medidata 5 RAVE Database Project'
 #grab the current git tag from repo
-var_directory_git_repo = 'C:\\Users\\jong\\Documents\\GitHub\\Rave'
-repo = git.Repo(var_directory_git_repo)
-g = git.Git(var_directory_git_repo)
+GIT_REPO_MAIN_DIRECTORY = 'C:\\Users\\jong\\Documents\\GitHub\\Rave'
+repo = git.Repo(GIT_REPO_MAIN_DIRECTORY)
+g = git.Git(GIT_REPO_MAIN_DIRECTORY)
 
 #pull current tag if necessary
 #current_tag = str(next((tag for tag in repo.tags if tag.commit == repo.head.commit), None))
@@ -28,35 +28,35 @@ def look_sql_all_tags():
     for tag in repo.tags:
         g.clean('-xdf')
         g.checkout(tag)
-        df = read_sql_files.read_sql_files_to_dataframe(var_directory_to_sql)
+        df = read_sql_files.read_sql_files_to_dataframe(SQL_DIRECTORY)
         df["tag"] = str(tag)
     
         if first_run == True:
             #write the dataframe to new table the first time
-            df.to_sql(sql_table,engine,if_exists='replace')
+            df.to_sql(SQL_TABLE, ENGINE, if_exists='replace')
             first_run = False
             break
         else:
             #after the first time append to the table
-            df.to_sql(sql_table,engine,if_exists='append')
+            df.to_sql(SQL_TABLE, ENGINE, if_exists='append')
 
 #pull single tag and append to existing table
 def look_sql_single_tag(tagname):
     g.clean('-xdf')
     g.checkout(tagname)
-    df = read_sql_files.read_sql_files_to_dataframe(var_directory_to_sql)
+    df = read_sql_files.read_sql_files_to_dataframe(SQL_DIRECTORY)
     df["tag"] = str(tagname)
-    df.to_sql(sql_table,engine,if_exists='append')
+    df.to_sql(SQL_TABLE, ENGINE, if_exists='append')
 
 
 #create sql connection
-connection = pyodbc.connect(db_connect_string)
+connection = pyodbc.connect(DB_CONNECT_STRING)
 cursor = connection.cursor()
 #truncate the table
-sqltruncate = ("""truncate table %s """ % sql_table)
+sqltruncate = ("""truncate table %s """ % SQL_TABLE)
 cursor.execute(sqltruncate)
 connection.commit()
-                
+
 #reload data into table
 #look_sql_all_tags()
 look_sql_single_tag('v2017.2.0')
